2dc2e02bd51cc3ec6e8e8ca8491ac7724bbbb362
MiczFlor <micz.flor@web.de>
2016-06-06 16:56:08 +0200

    added themes to repo

---
+++ modernreportcomfortable/theme_fonts.php
@@ -0,0 +1,19 @@
+/* 'Modern Report (Comfortable)' Version 0.2 theme fonts */
+        "roboto" => array(
+          'R' => "Roboto-Regular.ttf",
+          'B' => "Roboto-Bold.ttf",
+          'I' => "Roboto-Italic.ttf",
+          'BI' => "Roboto-BoldItalic.ttf",
+        ),
+        "ubuntu-mono" => array(
+          'R' => "UbuntuMono-R.ttf",
+          'B' => "UbuntuMono-B.ttf",
+          'I' => "UbuntuMono-RI.ttf",
+          'BI' => "UbuntuMono-BI.ttf",
+        ),
+        "roboto-light" => array(
+          'R' => "Roboto-Light.ttf",
+          'B' => "Roboto-Regular.ttf",
+          'I' => "Roboto-LightItalic.ttf",
+          'BI' => "Roboto-Italic.ttf",
+        ),
