449934316be9c06782ca34afbaccb6a95e75e8ea
MiczFlor <micz.flor@web.de>
2016-06-08 14:45:41 +0200

    new novel theme "new fiction"

---
+++ newfictioncompact/static/theme_fonts.php
@@ -0,0 +1,13 @@
+/* 'New Fiction (Compact)' Version 0.1 theme fonts */
+        "ubuntu" => array(
+          'R' => "Ubuntu-R.ttf",
+          'B' => "Ubuntu-B.ttf",
+          'I' => "Ubuntu-RI.ttf",
+          'BI' => "Ubuntu-BI.ttf",
+        ),
+        "ubuntu-mono" => array(
+          'R' => "UbuntuMono-R.ttf",
+          'B' => "UbuntuMono-B.ttf",
+          'I' => "UbuntuMono-RI.ttf",
+          'BI' => "UbuntuMono-BI.ttf",
+        ),
