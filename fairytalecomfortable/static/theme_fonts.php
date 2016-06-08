009ba3ca53ddd3bd0875e4f87ba9350c77f8aacd
MiczFlor <micz.flor@web.de>
2016-06-08 21:31:51 +0200

    adding two novel themes: fairy tale and new fiction

---
+++ fairytalecomfortable/static/theme_fonts.php
@@ -0,0 +1,13 @@
+/* 'Fairy Tale (Comfortable)' Version 0.1 theme fonts */
+        "texgyrepagella" => array(
+          'R' => "TeXGyrePagella-Regular.ttf",
+          'B' => "TeXGyrePagella-Bold.ttf",
+          'I' => "TeXGyrePagella-Italic.ttf",
+          'BI' => "TeXGyrePagella-BoldItalic.ttf",
+        ),
+        "texgyrecursor" => array(
+          'R' => "TeXGyreCursor-Regular.ttf",
+          'B' => "TeXGyreCursor-Bold.ttf",
+          'I' => "TeXGyreCursor-Italic.ttf",
+          'BI' => "TeXGyreCursor-BoldItalic.ttf",
+        ),
