cd0c71a528d42ca034189c00068a3efa406b26f3
MiczFlor <micz.flor@web.de>
2016-06-09 15:06:31 +0200

    adding php file for mpdf

---
+++ AA_fontconfig/theme_fonts.php
@@ -0,0 +1,83 @@
+<?php
+$fontsused = array(
+// contains all fonts only once, even if used in more than one theme. 
+// NOTE: this list can be used for the mpdf font config file.
+
+  "roboto" => array(
+    "R" => "Roboto-Regular.ttf",
+    "B" => "Roboto-Bold.ttf",
+    "I" => "Roboto-Italic.ttf",
+    "BI" => "Roboto-BoldItalic.ttf",
+  ),
+  "texgyrecursor" => array(
+    "R" => "TeXGyreCursor-Regular.ttf",
+    "B" => "TeXGyreCursor-Bold.ttf",
+    "I" => "TeXGyreCursor-Italic.ttf",
+    "BI" => "TeXGyreCursor-BoldItalic.ttf",
+  ),
+  "roboto-condensed" => array(
+    "R" => "RobotoCondensed-Regular.ttf",
+    "B" => "RobotoCondensed-Bold.ttf",
+    "I" => "RobotoCondensed-Italic.ttf",
+    "BI" => "RobotoCondensed-BoldItalic.ttf",
+  ),
+  "freemono" => array(
+    "R" => "FreeMono.ttf",
+    "B" => "FreeMonoBold.ttf",
+    "I" => "FreeMonoOblique.ttf",
+    "BI" => "FreeMonoBoldOblique.ttf",
+  ),
+  "dejavusans" => array(
+    "R" => "DejaVuSans.ttf",
+    "B" => "DejaVuSans-Bold.ttf",
+    "I" => "DejaVuSans-Oblique.ttf",
+    "BI" => "DejaVuSans-BoldOblique.ttf",
+  ),
+  "ubuntu-mono" => array(
+    "R" => "UbuntuMono-R.ttf",
+    "B" => "UbuntuMono-B.ttf",
+    "I" => "UbuntuMono-RI.ttf",
+    "BI" => "UbuntuMono-BI.ttf",
+  ),
+  "roboto-light" => array(
+    "R" => "Roboto-Light.ttf",
+    "B" => "Roboto-Regular.ttf",
+    "I" => "Roboto-LightItalic.ttf",
+    "BI" => "Roboto-Italic.ttf",
+  ),
+  "texgyreadventor" => array(
+    "R" => "TeXGyreAdventor-Regular.ttf",
+    "B" => "TeXGyreAdventor-Bold.ttf",
+    "I" => "TeXGyreAdventor-Italic.ttf",
+    "BI" => "TeXGyreAdventor-BoldItalic.ttf",
+  ),
+  "ubuntu" => array(
+    "R" => "Ubuntu-R.ttf",
+    "B" => "Ubuntu-B.ttf",
+    "I" => "Ubuntu-RI.ttf",
+    "BI" => "Ubuntu-BI.ttf",
+  ),
+  "ubuntu-light" => array(
+    "R" => "Ubuntu-L.ttf",
+    "B" => "Ubuntu-M.ttf",
+    "I" => "Ubuntu-LI.ttf",
+    "BI" => "Ubuntu-MI.ttf",
+  ),
+  "texgyrepagella" => array(
+    "R" => "TeXGyrePagella-Regular.ttf",
+    "B" => "TeXGyrePagella-Bold.ttf",
+    "I" => "TeXGyrePagella-Italic.ttf",
+    "BI" => "TeXGyrePagella-BoldItalic.ttf",
+  ),
+  "im_fell_dw_pica_pro" => array(
+    "R" => "IM_FELL_DW_Pica_PRO_Roman.ttf",
+    "I" => "IM_FELL_DW_Pica_PRO_Italic.ttf",
+  ),
+  "texgyreschola" => array(
+    "R" => "TeXGyreSchola-Regular.ttf",
+    "B" => "TeXGyreSchola-Bold.ttf",
+    "I" => "TeXGyreSchola-Italic.ttf",
+    "BI" => "TeXGyreSchola-BoldItalic.ttf",
+  ),
+);
+?>
---
+++ AA_fontconfig/theme_fonts_complete.php
@@ -0,0 +1,299 @@
+<?php
+$fontsused = array(
+// contains all fonts for all themes in one file. 
+// WARNING: there are doubles if the same font is used in more than one theme.
+
+/* 'Academic (Compact)' Version 0.1 theme fonts */
+        "roboto" => array(
+          'R' => "Roboto-Regular.ttf",
+          'B' => "Roboto-Bold.ttf",
+          'I' => "Roboto-Italic.ttf",
+          'BI' => "Roboto-BoldItalic.ttf",
+        ),
+        "texgyrecursor" => array(
+          'R' => "TeXGyreCursor-Regular.ttf",
+          'B' => "TeXGyreCursor-Bold.ttf",
+          'I' => "TeXGyreCursor-Italic.ttf",
+          'BI' => "TeXGyreCursor-BoldItalic.ttf",
+        ),
+        "roboto-condensed" => array(
+          'R' => "RobotoCondensed-Regular.ttf",
+          'B' => "RobotoCondensed-Bold.ttf",
+          'I' => "RobotoCondensed-Italic.ttf",
+          'BI' => "RobotoCondensed-BoldItalic.ttf",
+        ),
+/* 'Academic (Comfortable)' Version 0.1 theme fonts */
+        "roboto" => array(
+          'R' => "Roboto-Regular.ttf",
+          'B' => "Roboto-Bold.ttf",
+          'I' => "Roboto-Italic.ttf",
+          'BI' => "Roboto-BoldItalic.ttf",
+        ),
+        "freemono" => array(
+          'R' => "FreeMono.ttf",
+          'B' => "FreeMonoBold.ttf",
+          'I' => "FreeMonoOblique.ttf",
+          'BI' => "FreeMonoBoldOblique.ttf",
+        ),
+        "roboto-condensed" => array(
+          'R' => "RobotoCondensed-Regular.ttf",
+          'B' => "RobotoCondensed-Bold.ttf",
+          'I' => "RobotoCondensed-Italic.ttf",
+          'BI' => "RobotoCondensed-BoldItalic.ttf",
+        ),
+/* 'Artsy Bold (Comfortable)' Version 0.1 theme fonts */
+        "dejavusans" => array(
+          'R' => "DejaVuSans.ttf",
+          'B' => "DejaVuSans-Bold.ttf",
+          'I' => "DejaVuSans-Oblique.ttf",
+          'BI' => "DejaVuSans-BoldOblique.ttf",
+        ),
+        "ubuntu-mono" => array(
+          'R' => "UbuntuMono-R.ttf",
+          'B' => "UbuntuMono-B.ttf",
+          'I' => "UbuntuMono-RI.ttf",
+          'BI' => "UbuntuMono-BI.ttf",
+        ),
+        "roboto-light" => array(
+          'R' => "Roboto-Light.ttf",
+          'B' => "Roboto-Regular.ttf",
+          'I' => "Roboto-LightItalic.ttf",
+          'BI' => "Roboto-Italic.ttf",
+        ),
+/* 'Artsy Bold (Compact)' Version 0.1 theme fonts */
+        "dejavusans" => array(
+          'R' => "DejaVuSans.ttf",
+          'B' => "DejaVuSans-Bold.ttf",
+          'I' => "DejaVuSans-Oblique.ttf",
+          'BI' => "DejaVuSans-BoldOblique.ttf",
+        ),
+        "ubuntu-mono" => array(
+          'R' => "UbuntuMono-R.ttf",
+          'B' => "UbuntuMono-B.ttf",
+          'I' => "UbuntuMono-RI.ttf",
+          'BI' => "UbuntuMono-BI.ttf",
+        ),
+        "roboto-light" => array(
+          'R' => "Roboto-Light.ttf",
+          'B' => "Roboto-Regular.ttf",
+          'I' => "Roboto-LightItalic.ttf",
+          'BI' => "Roboto-Italic.ttf",
+        ),
+/* 'Bauhaus (Comfortable)' Version 0.1 theme fonts */
+        "texgyreadventor" => array(
+          'R' => "TeXGyreAdventor-Regular.ttf",
+          'B' => "TeXGyreAdventor-Bold.ttf",
+          'I' => "TeXGyreAdventor-Italic.ttf",
+          'BI' => "TeXGyreAdventor-BoldItalic.ttf",
+        ),
+        "texgyrecursor" => array(
+          'R' => "TeXGyreCursor-Regular.ttf",
+          'B' => "TeXGyreCursor-Bold.ttf",
+          'I' => "TeXGyreCursor-Italic.ttf",
+          'BI' => "TeXGyreCursor-BoldItalic.ttf",
+        ),
+/* 'Bauhaus (Compact)' Version 0.1 theme fonts */
+        "texgyreadventor" => array(
+          'R' => "TeXGyreAdventor-Regular.ttf",
+          'B' => "TeXGyreAdventor-Bold.ttf",
+          'I' => "TeXGyreAdventor-Italic.ttf",
+          'BI' => "TeXGyreAdventor-BoldItalic.ttf",
+        ),
+        "texgyrecursor" => array(
+          'R' => "TeXGyreCursor-Regular.ttf",
+          'B' => "TeXGyreCursor-Bold.ttf",
+          'I' => "TeXGyreCursor-Italic.ttf",
+          'BI' => "TeXGyreCursor-BoldItalic.ttf",
+        ),
+/* 'Common Sense (Comfortable)' Version 0.1 theme fonts */
+        "ubuntu" => array(
+          'R' => "Ubuntu-R.ttf",
+          'B' => "Ubuntu-B.ttf",
+          'I' => "Ubuntu-RI.ttf",
+          'BI' => "Ubuntu-BI.ttf",
+        ),
+        "ubuntu-mono" => array(
+          'R' => "UbuntuMono-R.ttf",
+          'B' => "UbuntuMono-B.ttf",
+          'I' => "UbuntuMono-RI.ttf",
+          'BI' => "UbuntuMono-BI.ttf",
+        ),
+        "ubuntu-light" => array(
+          'R' => "Ubuntu-L.ttf",
+          'B' => "Ubuntu-M.ttf",
+          'I' => "Ubuntu-LI.ttf",
+          'BI' => "Ubuntu-MI.ttf",
+        ),
+/* 'Common Sense (Compact)' Version 0.1 theme fonts */
+        "ubuntu" => array(
+          'R' => "Ubuntu-R.ttf",
+          'B' => "Ubuntu-B.ttf",
+          'I' => "Ubuntu-RI.ttf",
+          'BI' => "Ubuntu-BI.ttf",
+        ),
+        "ubuntu-mono" => array(
+          'R' => "UbuntuMono-R.ttf",
+          'B' => "UbuntuMono-B.ttf",
+          'I' => "UbuntuMono-RI.ttf",
+          'BI' => "UbuntuMono-BI.ttf",
+        ),
+        "ubuntu-light" => array(
+          'R' => "Ubuntu-L.ttf",
+          'B' => "Ubuntu-M.ttf",
+          'I' => "Ubuntu-LI.ttf",
+          'BI' => "Ubuntu-MI.ttf",
+        ),
+/* 'Fairy Tale (Comfortable)' Version 0.1 theme fonts */
+        "texgyrepagella" => array(
+          'R' => "TeXGyrePagella-Regular.ttf",
+          'B' => "TeXGyrePagella-Bold.ttf",
+          'I' => "TeXGyrePagella-Italic.ttf",
+          'BI' => "TeXGyrePagella-BoldItalic.ttf",
+        ),
+        "texgyrecursor" => array(
+          'R' => "TeXGyreCursor-Regular.ttf",
+          'B' => "TeXGyreCursor-Bold.ttf",
+          'I' => "TeXGyreCursor-Italic.ttf",
+          'BI' => "TeXGyreCursor-BoldItalic.ttf",
+        ),
+/* 'Fairy Tale (Compact)' Version 0.1 theme fonts */
+        "texgyrepagella" => array(
+          'R' => "TeXGyrePagella-Regular.ttf",
+          'B' => "TeXGyrePagella-Bold.ttf",
+          'I' => "TeXGyrePagella-Italic.ttf",
+          'BI' => "TeXGyrePagella-BoldItalic.ttf",
+        ),
+        "texgyrecursor" => array(
+          'R' => "TeXGyreCursor-Regular.ttf",
+          'B' => "TeXGyreCursor-Bold.ttf",
+          'I' => "TeXGyreCursor-Italic.ttf",
+          'BI' => "TeXGyreCursor-BoldItalic.ttf",
+        ),
+/* 'Historical Fiction (Comfortable)' Version 0.1 theme fonts */
+        "im_fell_dw_pica_pro" => array(
+          'R' => "IM_FELL_DW_Pica_PRO_Roman.ttf",
+          'I' => "IM_FELL_DW_Pica_PRO_Italic.ttf",
+        ),
+        "texgyrecursor" => array(
+          'R' => "TeXGyreCursor-Regular.ttf",
+          'B' => "TeXGyreCursor-Bold.ttf",
+          'I' => "TeXGyreCursor-Italic.ttf",
+          'BI' => "TeXGyreCursor-BoldItalic.ttf",
+        ),
+/* 'Historical Fiction (Compact)' Version 0.1 theme fonts */
+        "im_fell_dw_pica_pro" => array(
+          'R' => "IM_FELL_DW_Pica_PRO_Roman.ttf",
+          'I' => "IM_FELL_DW_Pica_PRO_Italic.ttf",
+        ),
+        "texgyrecursor" => array(
+          'R' => "TeXGyreCursor-Regular.ttf",
+          'B' => "TeXGyreCursor-Bold.ttf",
+          'I' => "TeXGyreCursor-Italic.ttf",
+          'BI' => "TeXGyreCursor-BoldItalic.ttf",
+        ),
+/* 'Modern Report (Comfortable)' Version 0.2 theme fonts */
+        "roboto" => array(
+          'R' => "Roboto-Regular.ttf",
+          'B' => "Roboto-Bold.ttf",
+          'I' => "Roboto-Italic.ttf",
+          'BI' => "Roboto-BoldItalic.ttf",
+        ),
+        "ubuntu-mono" => array(
+          'R' => "UbuntuMono-R.ttf",
+          'B' => "UbuntuMono-B.ttf",
+          'I' => "UbuntuMono-RI.ttf",
+          'BI' => "UbuntuMono-BI.ttf",
+        ),
+        "roboto-light" => array(
+          'R' => "Roboto-Light.ttf",
+          'B' => "Roboto-Regular.ttf",
+          'I' => "Roboto-LightItalic.ttf",
+          'BI' => "Roboto-Italic.ttf",
+        ),
+/* 'Modern Report (Compact)' Version 0.2 theme fonts */
+        "roboto" => array(
+          'R' => "Roboto-Regular.ttf",
+          'B' => "Roboto-Bold.ttf",
+          'I' => "Roboto-Italic.ttf",
+          'BI' => "Roboto-BoldItalic.ttf",
+        ),
+        "ubuntu-mono" => array(
+          'R' => "UbuntuMono-R.ttf",
+          'B' => "UbuntuMono-B.ttf",
+          'I' => "UbuntuMono-RI.ttf",
+          'BI' => "UbuntuMono-BI.ttf",
+        ),
+        "roboto-light" => array(
+          'R' => "Roboto-Light.ttf",
+          'B' => "Roboto-Regular.ttf",
+          'I' => "Roboto-LightItalic.ttf",
+          'BI' => "Roboto-Italic.ttf",
+        ),
+/* 'New Fiction (Comfortable)' Version 0.1 theme fonts */
+        "ubuntu" => array(
+          'R' => "Ubuntu-R.ttf",
+          'B' => "Ubuntu-B.ttf",
+          'I' => "Ubuntu-RI.ttf",
+          'BI' => "Ubuntu-BI.ttf",
+        ),
+        "ubuntu-mono" => array(
+          'R' => "UbuntuMono-R.ttf",
+          'B' => "UbuntuMono-B.ttf",
+          'I' => "UbuntuMono-RI.ttf",
+          'BI' => "UbuntuMono-BI.ttf",
+        ),
+/* 'New Fiction (Compact)' Version 0.1 theme fonts */
+        "ubuntu" => array(
+          'R' => "Ubuntu-R.ttf",
+          'B' => "Ubuntu-B.ttf",
+          'I' => "Ubuntu-RI.ttf",
+          'BI' => "Ubuntu-BI.ttf",
+        ),
+        "ubuntu-mono" => array(
+          'R' => "UbuntuMono-R.ttf",
+          'B' => "UbuntuMono-B.ttf",
+          'I' => "UbuntuMono-RI.ttf",
+          'BI' => "UbuntuMono-BI.ttf",
+        ),
+/* 'Novel Classic (Comfortable)' Version 0.1 theme fonts */
+        "texgyreschola" => array(
+          'R' => "TeXGyreSchola-Regular.ttf",
+          'B' => "TeXGyreSchola-Bold.ttf",
+          'I' => "TeXGyreSchola-Italic.ttf",
+          'BI' => "TeXGyreSchola-BoldItalic.ttf",
+        ),
+        "texgyrecursor" => array(
+          'R' => "TeXGyreCursor-Regular.ttf",
+          'B' => "TeXGyreCursor-Bold.ttf",
+          'I' => "TeXGyreCursor-Italic.ttf",
+          'BI' => "TeXGyreCursor-BoldItalic.ttf",
+        ),
+/* 'Novel Classic (Compact)' Version 0.1 theme fonts */
+        "texgyreschola" => array(
+          'R' => "TeXGyreSchola-Regular.ttf",
+          'B' => "TeXGyreSchola-Bold.ttf",
+          'I' => "TeXGyreSchola-Italic.ttf",
+          'BI' => "TeXGyreSchola-BoldItalic.ttf",
+        ),
+        "texgyrecursor" => array(
+          'R' => "TeXGyreCursor-Regular.ttf",
+          'B' => "TeXGyreCursor-Bold.ttf",
+          'I' => "TeXGyreCursor-Italic.ttf",
+          'BI' => "TeXGyreCursor-BoldItalic.ttf",
+        ),
+/* 'Typewriter (Comfortable)' Version 0.1 theme fonts */
+        "texgyrecursor" => array(
+          'R' => "TeXGyreCursor-Regular.ttf",
+          'B' => "TeXGyreCursor-Bold.ttf",
+          'I' => "TeXGyreCursor-Italic.ttf",
+          'BI' => "TeXGyreCursor-BoldItalic.ttf",
+        ),
+/* 'Typewriter (Compact)' Version 0.1 theme fonts */
+        "texgyrecursor" => array(
+          'R' => "TeXGyreCursor-Regular.ttf",
+          'B' => "TeXGyreCursor-Bold.ttf",
+          'I' => "TeXGyreCursor-Italic.ttf",
+          'BI' => "TeXGyreCursor-BoldItalic.ttf",
+        ),
+);
+?>
